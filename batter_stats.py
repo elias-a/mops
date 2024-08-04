9ad9bd6d973c1b2e5dd32d9693daf6b8bca3b329
elias-a <elias.alstead@gmail.com>
2024-08-04 16:45:51 -0400

    Remove unused file

--- batter_stats.py
+++
@@ -1,6 +0,0 @@
-
-
-def metric_plus(entity_stats, parent_stats):
-    obp, slg, _ = entity_stats
-    parent_obp, parent_slg, _ = parent_stats
-    return 100 * ((obp / parent_obp) + (slg / parent_slg) - 1)
